6be741ab1024c0247dd0fd3c134e76ee49782047
Philip Obalo <philipobalojr@gmail.com>
2022-12-09 18:04:03 -0500

    commented out the shell script

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-systemctl status jenkins ## or systemctl status jenkins | grep Active
+##sudo yum update –y
+##sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+##sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+##sudo yum upgrade
+##sudo amazon-linux-extras install java-openjdk11 -y
+##sudo yum install jenkins -y
+##sudo systemctl enable jenkins
+##sudo systemctl start jenkins
+##systemctl status jenkins ## or systemctl status jenkins | grep Active
 
 ## Installing Git
-sudo yum install git -y
+##sudo yum install git -y
 
 ## connecting to jenkins
---http://<your_server_public_DNS>:8080
+##--http://<your_server_public_DNS>:8080
 
 ## Geting admin password for jenkins
---sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+##--sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
